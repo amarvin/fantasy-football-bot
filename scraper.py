e7cbd1fc2201d42f4e86185eb89304e35b8a493c
Alex Marvin <alex.marvin@gmail.com>
2018-11-24 11:53:41 -0500

    Removes unused imports

--- scraper.py
+++ scraper.py
@@ -6,10 +6,6 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
 
