995aafdac8b31d254f9c7b3c65e324b648c63bc0
Alex Marvin <alex.marvin@gmail.com>
2019-11-18 22:16:44 -0500

    v1.0.8

--- ffbot/constants.py
+++ ffbot/constants.py
@@ -1,4 +1,4 @@
-VERSION = '1.0.7'
+VERSION = '1.0.8'
 
 USER_AGENTS = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
