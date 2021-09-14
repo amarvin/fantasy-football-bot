e7ad648d7fe9a6498a14da735067eaf52168c278
Alex Marvin <alex.marvin@gmail.com>
2021-09-13 23:59:40 -0400

    Disables linting "unused import"

--- ffbot/__init__.py
+++ ffbot/__init__.py
@@ -1,6 +1,6 @@
 from .constants import VERSION
-from .optimizer import optimize
-from .scraper import current_week, scrape
-from .utils import save, load
+from .optimizer import optimize  # noqa: F401,E402
+from .scraper import current_week, scrape  # noqa: F401,E402
+from .utils import save, load  # noqa: F401,E402
 
 __version__ = VERSION
