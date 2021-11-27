d29334b6be839a5c1310868588ef53ef7fe08d37
Alex Marvin <alex.marvin@gmail.com>
2021-11-26 23:25:14 -0500

    Adds test_optimizer (#49)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
 from os.path import join
 
+POSITIONS = "QB, WR, WR, WR, RB, RB, TE, W/R/T, K, DEF, BN, BN, BN, BN, IR"
 SCRAPER_FILE = join("tests", "1901-01-01 0100 week 4.csv")
+TEAM = 8
---
+++ tests/test_optimizer.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from . import POSITIONS, SCRAPER_FILE, TEAM
+import ffbot
+
+
+def test_optimize():
+    df, week = ffbot.load(SCRAPER_FILE)
+    assert week == 4
+    df_opt = ffbot.optimize(df, week, TEAM, POSITIONS)
+    desired_df_opt = """                                Add              Drop Total points Discounted points     VOR
+0                                     Player 200 (WR)                                       
+1                  <current roster>                      +1,723.27           +420.20 +515.90
+2                   Player 221 (TE)                          +6.11             +0.58  -19.01
+3                  Player 143 (DEF)   Player 101 (QB)       -10.41             +2.33  -16.19
+4                    Player 157 (K)    Player 177 (K)        +4.06             +2.01   +4.06
+5                  Player 162 (DEF)  Player 140 (DEF)        -1.62             +0.64   -5.79
+6                    Player 135 (K)    Player 59 (RB)        +9.26             +0.37  -39.68
+7  Player 90 (WR) - Waivers (Jan 2)                          +9.45             +3.23  +17.22
+8                                     Player 132 (WR)        +0.00             +0.00   +0.00
+9      Player 129 (QB) - Free Agent   Player 174 (WR)       +14.53             +0.85  +11.16"""  # noqa: W291
+    with pd.option_context("display.float_format", "{:+,.2f}".format):
+        assert df_opt.to_string() == desired_df_opt
