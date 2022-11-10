99ccd52447eb2ad7040fb61157a26d1d5b941266
Alex Marvin <alex.marvin@gmail.com>
2022-11-10 00:17:17 -0500

    Adds support for IDP leagues

--- demo.py
+++ demo.py
@@ -9,6 +9,8 @@ week = ffbot.current_week()
 
 # Scrape data for current and available players, and their point forecasts for each week
 df = ffbot.scrape(LEAGUE)
+# If playing an Individual Defensive Player (IDP) league, then scrape additional players with:
+# df = ffbot.scrape(LEAGUE, is_IDP=True)
 
 # Optional save data to CSV, and load latest data
 # ffbot.save(df, week)
