23deacdc40085ee734e6d3346f3037a77b59d59a
Alex Marvin <alex.marvin@gmail.com>
2024-03-18 22:56:34 -0500

    Re-adds whitespace

--- tests/test_optimizer.py
+++ tests/test_optimizer.py
@@ -11,7 +11,7 @@ def test_optimize():
     df_opt = ffbot.optimize(df, week, TEAM, POSITIONS)
     # fmt: off
     desired_df_opt = """                                Add              Drop Total points Discounted points    VOR
-0                                     Player 200 (WR)
+0                                     Player 200 (WR)                                      
 1                  <current roster>                        1723.27             420.2  515.9
 2                   Player 221 (TE)                           6.11              0.58 -19.01
 3                  Player 143 (DEF)   Player 101 (QB)       -10.41              2.33 -16.19
@@ -20,6 +20,6 @@ def test_optimize():
 6                    Player 135 (K)    Player 59 (RB)         9.26              0.37 -39.68
 7  Player 90 (WR) - Waivers (Jan 2)                           9.45              3.23  17.22
 8                                     Player 132 (WR)          0.0               0.0    0.0
-9      Player 129 (QB) - Free Agent   Player 174 (WR)        14.53              0.85  11.16"""
+9      Player 129 (QB) - Free Agent   Player 174 (WR)        14.53              0.85  11.16"""  # noqa: W291
     # fmt: on
     assert df_opt.to_string() == desired_df_opt
